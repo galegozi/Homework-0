012324f25ac85761e1c517f8f8aa42f7d842c89b
githubgal <geegozi@gmail.com>
2016-09-20 12:55:34 -0500

    Homework 0
    The first homework for cosc 1430

--- Homework 0/main.cpp
+++ Homework 0/main.cpp
@@ -8,8 +8,24 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    int run = 0;
+    cout << "How many for loop run do you want?\n";
+    cout << "Enter a number: ";
+    cin >> run;
+    for(int x = 1; x <= run; x++)
+        cout << "For loop number: " << x << endl;
+    cout << "How many while loop run do you want?\n";
+    cout << "Enter a number: ";
+    cin >> run;
+    int x = 1;
+    while (x <= run)
+    {
+        cout << "While loop number: " << x << endl;
+        x++;
+    }
     return 0;
 }
